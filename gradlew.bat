f94b40c7f545df130809905a17443936dbeaea14
13113025529 <105544382+13113025529@users.noreply.github.com>
2023-01-13 06:27:26 +0000

    Drivers: hv: vmbus: incorrect device name is printed when child device is unregistered

---
+++ gradlew.bat
@@ -0,0 +1,10 @@
+
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
+
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
