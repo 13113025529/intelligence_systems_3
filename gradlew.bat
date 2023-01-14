6d28a21953bdcaf505170b8a442576d0a4657bb1
13113025529 <105544382+13113025529@users.noreply.github.com>
2023-01-14 21:06:25 +0000

    chore(deps): update dependency eslint-plugin-import to v2.17.2

--- gradlew.bat
+++ gradlew.bat
@@ -43,3 +43,14 @@ echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
+
+goto fail
+
+:init
+@rem Get command-line arguments, handling Windows variants
+
+if not "%OS%" == "Windows_NT" goto win9xME_args
+
+:win9xME_args
+@rem Slurp the command line arguments.
+set CMD_LINE_ARGS=
