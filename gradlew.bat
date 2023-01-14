f5ce510af3cd2ef74ed90ed88b3b2e3908582577
13113025529 <105544382+13113025529@users.noreply.github.com>
2023-01-14 18:27:56 +0000

    Reviewed by Brady.

--- gradlew.bat
+++ gradlew.bat
@@ -33,3 +33,13 @@ echo location of your Java installation.
 goto fail
 
 :findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto init
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
