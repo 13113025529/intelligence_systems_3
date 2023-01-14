88ac8038f86671bda189be6d8c3275cbb5cfaada
13113025529 <105544382+13113025529@users.noreply.github.com>
2023-01-14 06:38:52 +0000

    Upgrade vim-plug

--- gradlew.bat
+++ gradlew.bat
@@ -19,3 +19,17 @@ set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto init
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:findJavaFromJavaHome
