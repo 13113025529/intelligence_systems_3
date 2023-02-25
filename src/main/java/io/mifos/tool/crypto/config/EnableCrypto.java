8ed58678e803c586554369900c52a06fd8216240
13113025529 <105544382+13113025529@users.noreply.github.com>
2023-02-25 20:17:10 +0000

    Updated metadata

--- src/main/java/io/mifos/tool/crypto/config/EnableCrypto.java
+++ src/main/java/io/mifos/tool/crypto/config/EnableCrypto.java
@@ -14,3 +14,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package io.mifos.tool.crypto.config;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
