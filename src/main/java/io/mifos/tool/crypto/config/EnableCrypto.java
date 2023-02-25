8f307107f05fd8c932bb2e0334b91b02c157dd53
13113025529 <105544382+13113025529@users.noreply.github.com>
2023-02-25 22:49:21 +0000

    Pontoon: Update Sorbian, Lower (dsb) localization of Test Pilot Website

--- src/main/java/io/mifos/tool/crypto/config/EnableCrypto.java
+++ src/main/java/io/mifos/tool/crypto/config/EnableCrypto.java
@@ -23,3 +23,14 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import({
+    CryptoSpringConfiguration.class
+})
+public @interface EnableCrypto {
+}
