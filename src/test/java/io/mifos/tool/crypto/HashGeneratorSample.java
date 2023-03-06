1cda3668b50415e0f38afaa0c80599310d349e6f
13113025529 <105544382+13113025529@users.noreply.github.com>
2023-03-06 15:50:08 +0000

    Fix broken tests

--- src/test/java/io/mifos/tool/crypto/HashGeneratorSample.java
+++ src/test/java/io/mifos/tool/crypto/HashGeneratorSample.java
@@ -12,3 +12,13 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.mifos.tool.crypto;
+
+import io.mifos.tool.crypto.config.EnableCrypto;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
